5779562ce815ac7bf63e39545751b8ea095e0634
hkailee <leehongkai@gmail.com>
2017-09-15 00:51:45 +0800

    update

--- utils_query.py
+++ utils_query.py
@@ -1,3 +1,36 @@
 #!/usr/local/bin/python3
 
-import sqlite3, ast
+import sqlite3
+
+def login(dbfile):
+    conn = sqlite3.connect(dbfile)  # create or open db file
+    curs = conn.cursor()
+    return conn, curs
+
+def makedb(dbfile, table, columnFeatures):
+    #columnFeatures = input("eg: (Column1 char(30), Column2 char(10), Column3 int(4))")
+    conn, curs = login(dbfile)
+    try:
+        curs.execute('drop table ' + table)
+        print('Drop table ' + table)
+    except:
+        print('database table did not exist')
+    command = 'create table %s %s' % (table, columnFeatures)
+    curs.execute(command)
+    conn.commit()
+    
+def loaddb(table, dbfile, datafile, conn=None, verbose=True):
+    conn, curs = login(dbfile)
+    file = open(datafile)
+    rows = [line.rstrip().split('\t') for line in file]  # [[x,x,x], [x,x,x]]
+    rows = [str(tuple(rec)) for rec in rows[1:]]            # ["(x,x,x)", "(x,x,x)"]
+    for recstr in rows:
+        curs.execute('insert into ' + table + ' values ' + recstr)
+    if conn:
+        conn.commit()
+    if verbose:
+        print(len(rows), 'rows loaded')
+        
+def cleardb(dbfile, table):
+    conn, curs = login(dbfile)
+    curs.execute('drop table ' + table)
